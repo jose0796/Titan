bce01d2ed0d456cb81cd0d203a6d6bca3d242791
jose0796 <jmoran071996@gmail.com>
2018-05-05 22:09:39 -0400

    Revert "Modify pc_tb.v"
    This reverts commit 12f7d603c69239491317aa78683df28280cff9ec.

--- tests/pc_tb.v
+++ tests/pc_tb.v
@@ -10,9 +10,22 @@ module testbench(
 		 repeat(5)  
 		 	@(posedge clk_i) #5; 
 		 rst_o = 0; 
-		 repeat(10) begin
+		 repeat(4) begin
 			 @(posedge clk_i) $display("pc = %h", pc_i);
 		 end
+//		 if (pc_i == 32'h00000008) begin
+//			 $display("test 1: passed");
+//		 end  else begin
+//			 $display("test 1: failed, pc = %h", pc_i); 
+//		 end
+//		 repeat(2) begin
+//			 @(posedge clk_i) $display("pc_i = %d", pc_i); 
+//		 end
+//		 if (pc_i == 32'd8) begin 
+//			 $display("test 2: passed"); 
+//		end else begin 
+//			$display("test 2: failed, pc = %h", pc_i); 
+//		end
 		$finish; 
 	end 
 
