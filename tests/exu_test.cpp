2981007f0bdcbdc60c8a67cce98d1770af03bea5
jose0796 <jmoran071996@gmail.com>
2018-05-30 14:35:00 +0100

    Redoing test for EXU

--- tests/exu_test.cpp
+++ tests/exu_test.cpp
@@ -10,9 +10,6 @@
 #include "testbench.h"
 
 
-#define OK_COLOR    "\033[0;32m"
-#define ERROR_COLOR "\033[0;31m"
-#define NO_COLOR    "\033[m"
 
 #define PA      0
 #define PB      1
@@ -36,73 +33,58 @@
 
 using namespace std;
 
-class SIMULATIONTB: public Testbench<Vexu> {
+class TB: public Testbench<Vexu> {
   public:
-    // -----------------------------------------------------------------------------
-    // Testbench constructor
-    SIMULATIONTB(double frequency=1e6, double timescale=1e-9): Testbench(frequency, timescale) {}
+    int ok = 0;
+    TB(double frequency=1e9, double timescale=1e-10): Testbench(frequency, timescale) {}
 
-    int Simulate(unsigned long max_time=1000000){
-      Reset();
+    int Simulate(unsigned long max_time=100000000){
 
       //|  port_a | port_b |    ALU OP  | Output |
-      int data[TOTAL_TESTS][7] = {                      //           									
- 	{          5,     6,   ALU_ADD,         11},    // 5 + 6 = 11                     
-        {        -20,     5,   ALU_ADD,        -15},    // -20 + 5 = -15                 
-        {       -187,   187,   ALU_ADD,          0},    // -187 + 187 = 0               
-        {          4,     3,   ALU_SUB,          1},    // 4 - 3 = 1                    
-        {        -23,  -127,   ALU_SUB,        104},    // -23 - (-127) = 104           
-        {         -1,     0,   ALU_SUB,         -1},    // -1 - 0 = -1                 
-        {        120,   245,   ALU_AND,        112},    // 245 & 120 = 112                
-        {        540,   540,   ALU_AND,        540},    // 540 & 540 = 540               
-        {        100,    50,   ALU_OR,         118},    // 100 | 50 = 118              
-        {          0,     0,   ALU_OR,           0},    // 0 | 0 = 0                      
-        {        573,   215,   ALU_XOR,        746},    // 573 ^ 215 = 746                
-        {       4500,  4500,   ALU_XOR,          0},    // 4500 ^ 4500 = 0                
-        {       1024,     3,   ALU_SRL,        128},    // 1024 >> 4 = 128                
-        {-2147483648,     5,   ALU_SRL,   67108864},    // -2147483648 >> 5 = 67108864 
-        {-2147483648,     5,   ALU_SRA,  -67108864},    // -2147483648 >> 5 = -67108864   
-        {       2048,     1,   ALU_SRA,       1024},    // 2048 >> 1 = 1024               
-        {         64,     5,   ALU_SLL,       2048},    // 64 << 5 = 2048                 
-        {         -2,     1,   ALU_SLL,         -4},    // -2 << 1 = -4                
-        {          1,    10,   ALU_SLT,          1},    // 1 < 10 = true                  
-        {         20,    10,   ALU_SLT,          0},    // 20 < 10 = false              
-        {        -20,   -10,   ALU_SLT,          1},    // -20 < -10 = true               
-        {         50,   800,   ALU_SLTU,         1},    // 50 < 800 = true (Unsigned)     
-        {         30,    20,   ALU_SLTU,         0},    // 30 < 20 = false (Unsigned)     
-        {        100,  -200,   ALU_SLTU,         1}};   // 100 < -200 = true (Unsigned)  
+      
+	int operations[10] = { ALU_ADD, ALU_AND, ALU_OR, ALU_SUB, ALU_XOR, ALU_SLL, ALU_SRA, ALU_SRL, ALU_SLT, ALU_SLTU };
+	unsigned int data_i[20][2]  = {
+	{1,1},{3,7},{0x80000000,0xffff8000},{0x7ffff000,0xffff8000},{0x7ffff,1},
+	{0xff00ff00,0xf0f0f0f0},{0xff00f0,0xf0f0f0f0},{0xff00ff,0xf0f0f0f},{0xf00ff00f,0xf0f0f0f0},{0xff00ff00,0xf0f0f0f},
+	{0xff00ff00,0xf0f0f0f},{0xff00ff0,0xf0f0f0f0},{0xff00ff,0xf0f0f0f},{0xf00ff00f,0xf0f0f0f0},{0xff00ff00,0xf0f0f0f},
+	{1,1},{3,7},{0x80000000,0xffff8000},{0x7fff,0xffff8},{0x7fffffff,0xf777}	};
 
-      int num_test;
-      for (num_test = 0; num_test < TOTAL_TESTS; num_test++) {
-        m_core->port_a = data[num_test][PA];
-        m_core->port_b = data[num_test][PB];
-        m_core->alu_op  = data[num_test][ALU_OP] ;
+	unsigned int data_o[20]= {
+	  	 2,10,0x7fff8000,0x7fff7000,0x80000, // add op
+		 0xf000f000, 0xf000f0, 0xf000f,0xf000f000,0xf000f00, // and op 
+		 0xff0fff0f, 0xfff0fff0,0xfff0fff,0xf0fff0ff,0xff0fff0f, //or op
+		 0,0xfffffffb,0x80008000,0xffff8001,0x7fff8 //sub op
+	}; 
+      for (int i  = 0; i < 10; i++) {
+        m_core->port_a = data_i[i][0];
+        m_core->port_b = data_i[i][1];
+        m_core->alu_op = operations[int(i/5)];
 
         Tick();
 
-        if((m_core->result != data[num_test][OUT]))
-          return num_test;
+        if((m_core->result != data_o[i])) {
+          	 ok = -1;
+		 break;
+	}
       }
-      return num_test;
+	if ( ok == 0){
+		printf("\nEXU Testbench:\n");
+		printf("Test Passed! ");
+	}
+  	else{
+	  	printf("Test Failed! ");
+	}
     }
 };
 
 
 int main(int argc, char **argv, char **env) {
-  std::unique_ptr<SIMULATIONTB> tb(new SIMULATIONTB());
+  std::unique_ptr<TB> tb(new TB());
 
   tb->OpenTrace("exu_test.vcd");
 
-  int ret = tb->Simulate();
-
-  printf("\nEXU Testbench:\n");
-
-  if(ret == TOTAL_TESTS)
-    printf(OK_COLOR " Test Passed! " NO_COLOR);
-  else
-    printf(ERROR_COLOR  " Test Failed! " NO_COLOR);
-
-  printf("Complete: %d/%d\n", ret, TOTAL_TESTS);
+  tb->Reset();
+  int n = tb->Simulate();
 
   exit(0);
 }
